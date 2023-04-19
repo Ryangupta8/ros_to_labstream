44df5d38a5b607c8395c40e165382e90130d8465
Ryan Gupta <ryangupta8@gmail.com>
2023-04-19 13:27:55 -0500

    :working a1 state (spot state) from bag file to HS computer

--- scripts/ros_to_labstream.py
+++ scripts/ros_to_labstream.py
@@ -25,6 +25,7 @@ class Listener:
         self.hsr_pose.orientation.y = data.pose.pose.orientation.y
         self.hsr_pose.orientation.z = data.pose.pose.orientation.z
         self.hsr_pose.orientation.w = data.pose.pose.orientation.w
+        print("hsr callkback")
 
     def spot_pose_callback(self, data):
         self.spot_pose.position.x = data.pose.pose.position.x
@@ -33,6 +34,7 @@ class Listener:
         self.spot_pose.orientation.y = data.pose.pose.orientation.y
         self.spot_pose.orientation.z = data.pose.pose.orientation.z
         self.spot_pose.orientation.w = data.pose.pose.orientation.w
+        print("spot callkback")
 
 
 def main():
@@ -43,8 +45,7 @@ def main():
     name2 = 'hsr_state'
     type = 'xy_rxryrzrw'
     n_channels = 6
-    r = rospy.Rate(10) # 10hz
-
+    
     info1 = StreamInfo(name1, type, n_channels, srate, 'float32', 'spot_state')
     info2 = StreamInfo(name2, type, n_channels, srate, 'float32', 'hsr_state')
 
@@ -52,25 +53,32 @@ def main():
     outlet2 = StreamOutlet(info2)
 
     rospy.init_node('ros_to_labstream')
+    r = rospy.Rate(10) # 10hz
 
     rospy.Subscriber("/hsr/global_pose", PoseWithCovarianceStamped, listener.hsr_pose_callback)
     rospy.Subscriber("/a1_950/global_pose", PoseWithCovarianceStamped, listener.spot_pose_callback)
+    curr_time = time.clock()
+    prev_time = time.clock()
 
     while not rospy.is_shutdown():
 
+        curr_time = time.clock()
 
+        # mysample1 = [rand() for _ in range(n_channels)]
         mysample1 = [listener.spot_pose.position.x, listener.spot_pose.position.y,
                 listener.spot_pose.orientation.x, listener.spot_pose.orientation.y,
                 listener.spot_pose.orientation.z, listener.spot_pose.orientation.w]
         # print("mysample = ", mysample1)
-        mysample2 = [listener.hsr_pose.position.x, listener.hsr_pose.position.y,
-                listener.hsr_pose.orientation.x, listener.hsr_pose.orientation.y,
-                listener.hsr_pose.orientation.z, listener.hsr_pose.orientation.w]
+        mysample2 = [rand() for _ in range(n_channels)]
+        # mysample2 = [listener.hsr_pose.position.x, listener.hsr_pose.position.y,
+        #         listener.hsr_pose.orientation.x, listener.hsr_pose.orientation.y,
+        #         listener.hsr_pose.orientation.z, listener.hsr_pose.orientation.w]
 
-        outlet1.push_sample(mysample1)
-        outlet2.push_sample(mysample2)
-
-        r.sleep()
+        if curr_time - prev_time >= 0.08:
+            print("output")
+            outlet1.push_sample(mysample1)
+            outlet2.push_sample(mysample2)
+            prev_time = curr_time
 
 
 if  __name__ == '__main__':
